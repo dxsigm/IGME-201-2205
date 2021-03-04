51a82442ca0cb073cb79780620ec0fa89140715f
dxsigm <54343067+dxsigm@users.noreply.github.com>
2021-03-04 13:40:37 -0500

    Session 6-2

---
+++ Week 06/Session 2/ClassCopy/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ch09Ex03
+{
+    class MyContent
+    {
+        public string myString;
+
+        public System.Object GetCopy()
+        {
+            // this will create a new MyContent object and copy myString
+            // from this into the new object
+            // and return the new object as System.Object
+            return MemberwiseClone();
+        }
+    }
+
+    // inherit from the ICloneable interface to support deep copy
+    class MyClass : ICloneable
+    {
+        // myContainingClass is a reference data type
+        public MyContent myContainingClass = new MyContent();
+
+        // val is a value data type
+        public int val;
+
+        // intArray is a reference data type
+        public List<int> intArray = new List<int>();
+
+        // the Clone() method is required by the interface
+        // and should implement the deep copy of this class
+        public System.Object Clone()
+        {
+            // first do a MemberwiseClone() of the top level of the class
+            // this is a shallow copy which creates a new object and only copies the val field
+            // myContainingClass and intArray will be copied by reference
+            MyClass copy = (MyClass)this.MemberwiseClone();
+
+            // call the MemberwiseClone() of the child class
+            // by calling its GetCopy() method
+            // note that MemberwiseClone is "protected" therefore it can only
+            // be called within its own class
+            // ie. we cannot do:
+            // copy.myContainingClass = (MyContent)this.myContainingClass.MemberwiseClone();
+            // MemberwiseClone() and Clone() return System.Object, therefore we need
+            // to explicitly cast the object to the correct type
+            copy.myContainingClass = (MyContent)this.myContainingClass.GetCopy();
+
+            // GetRange() creates a new shoebox copy of a List<>
+            copy.intArray = this.intArray.GetRange(0, this.intArray.Count);
+
+            // return copy which is a MyClass object as System.Object
+            // note that object = copy is valid (any class type can be referenced as an object)
+            // but copy = object is invalid (any object cannot be any class type)
+            // for example, a Ming vase can be easily turned into an object (if you drop it for example)
+            //       object = Ming vase  (valid)
+            // but any object cannot be a Ming vase!
+            //       Ming vase = object (invalid)
+            return copy;
+        }
+    }
+
+    struct myStruct
+    {
+        public int val;
+    }
+
+    class ClassCopy
+    {
+        static void Main(string[] args)
+        {
+            MyClass objectA = new MyClass();
+            objectA.val = 10;
+            objectA.myContainingClass.myString = "david";
+
+            // if we want to copy objectA into objectB
+            MyClass objectB;
+
+            // this is NOT the way to do it!
+            // classes are reference data types
+            // both variables are pointing to the same object
+            // there is only 1 shoebox which was created by objectA
+            objectB = objectA;
+
+            // one way is to create the new object
+            objectB = new MyClass();
+
+            // and explicitly copy every class member
+            objectB.val = objectA.val;
+
+            // and copy each member of the containing class
+            objectB.myContainingClass.myString = objectA.myContainingClass.myString;
+
+            // deep copy uses the ICloneable interface
+            // and depends on our creating a Clone() method in the class
+            // that does the field-by-field member copy
+            // create an interface variable
+            ICloneable cloneable;
+
+            // set the interface variable equal to the object to copy
+            cloneable = objectA;
+
+            // call the Clone() method and return the correct class type
+            objectB = (MyClass)cloneable.Clone();
+
+            // we could also just call Clone() via objectA
+            objectB = (MyClass)objectA.Clone();
+
+            // remember that structs are value data types!
+            myStruct structA = new myStruct();
+            myStruct structB = structA;
+
+            // structA and structB are separate shoeboxes
+            structA.val = 30;
+            structB.val = 40;
+
+            Console.WriteLine("objectA.val = {0}", objectA.val);
+            Console.WriteLine("objectA.myContainingClass.myString = {0}", objectA.myContainingClass.myString);
+            Console.WriteLine("objectB.val = {0}", objectB.val);
+            Console.WriteLine("objectB.myContainingClass.myString = {0}", objectB.myContainingClass.myString);
+            Console.WriteLine("structA.val = {0}", structA.val);
+            Console.WriteLine("structB.val = {0}", structB.val);
+        }
+    }
+
+}
---
+++ Week 06/Session 2/GameOfLife/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    // define possible alive states
+    public enum EAliveState
+    {
+        alive,
+        dead
+    }
+
+    // define possible infected states
+    public enum EInfectedState
+    {
+        infected,
+        vaccinated,
+        organic
+    }
+
+    // all neighbors of a cell
+    public enum EDirection
+    {
+        right,
+        down,
+        left,
+        up
+            // topleft
+            // topright
+            // bottomleft
+            // bottomright
+    }
+
+    // use a "by value" structure type to store the current and next state of each cell
+    public struct StructCellState
+    {
+        // the infected state
+        public EInfectedState eInfectedState;
+
+        // the private alive state in this state structure
+        private EAliveState eAliveState;
+
+        // a property to apply logic to the infected state based on changing the alive state
+        public EAliveState EAliveState
+        {
+            get
+            {
+                // return the current alive state
+                return this.eAliveState;
+            }
+
+            set
+            {
+                // update private alive state member
+                this.eAliveState = value;
+
+                // if the alive state is now dead
+                if ( this.eAliveState == EAliveState.dead)
+                {
+                    // reset the infected state to organic (its original state)
+                    this.eInfectedState = EInfectedState.organic;
+                }
+            }
+        }
+    }
+
+    static class Game
+    {
+
+        public class Cell
+        {
+            // the max # of infected and vaccinated cells we want in our organism
+            const int MAX_VIRUS = 50;
+            const int MAX_VACCINES = 50;
+
+            // dynamically calculate how many neighbors each cell has based on the number of directions in EDirection
+            public static int MAX_NEIGHBORS = Enum.GetNames(typeof(EDirection)).Length;
+
+            // the array of neighbor cells for this cell
+            public Cell[] neighbor;
+
+            // the next cell to the "right" of this cell.  
+            // This allows us to have all our cells in a 1D list which we will use in our recursive method below
+            public Cell nextCell;
+
+            // store the current and next cell states using our structures
+            // which are "by value" fields and allow using the "=" operator to copy one structure into another
+            // note that you need to use Deep or Shallow copying to copy class objects
+            // therefore structures can be more efficient storage
+            public StructCellState currentCellState;
+            public StructCellState nextCellState;
+
+            // the Cell constructor which accepts the total # of cells in the organism 
+            // and the probability for a cell to be alive when it is created
+            public Cell(int maxCells, int probability = 6)
+            {
+                // create the neighbor array for this cell
+                // this needs to be done in the constructor because MAX_NEIGHBORS is calculated at runtime
+                neighbor = new Cell[MAX_NEIGHBORS];
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+
+        // loop through the 2D organism array and output characters to indicate the current state of each cell
+        public static void PrintOrganism(Cell[,] organism, int maxRows, int maxCols)
+        {
+            string output = "----------------------------------------------------------------------------------\n";
+
+            for (int row = 0; row < maxRows; ++row)
+            {
+                output += "|";
+                for (int col = 0; col < maxCols; ++col)
+                {
+                    Cell thisCell = organism[row, col];
+                    if (thisCell.currentCellState.EAliveState == EAliveState.alive)
+                    {
+                        switch ((int)thisCell.currentCellState.eInfectedState)
+                        {
+                            case (int)EInfectedState.organic:
+                                output += "\x25cb";
+                                break;
+
+                            case (int)EInfectedState.vaccinated:
+                                output += "\x25ca";
+                                break;
+
+                            case (int)EInfectedState.infected:
+                                output += "\x25cf";
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        output += " ";
+                    }
+                }
+
+                output += "|\n";
+            }
+
+            output += "----------------------------------------------------------------------------------";
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+            Console.Write(output);
+        }
+    }
+}
