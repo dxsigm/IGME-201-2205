42f172e3a118f42826869db9d79aa7d8cfbd1d33
dxsigm <54343067+dxsigm@users.noreply.github.com>
2021-03-09 13:02:00 -0500

    Session 7-1

---
+++ Week 07/Session 1/CourseLib/Class1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLib
+{
+
+    //Class: Schedule
+    //Author: Allison Maus
+    //Purpose: create a schedule 
+    //restrictions: none
+    public class Schedule
+    {
+        //create field members
+        public DateTime startTime;
+        public DateTime endTime;
+        public List<DayOfWeek> daysOfWeek = new List<DayOfWeek>();
+
+    }
+
+    //Class: Courses
+    //Author: Allison Maus
+    //Purpose: create a sorted list of courseCodes
+    //restrictions: none
+    public class Courses
+    {
+        //initalize field members
+        // create a Sorted List indexed on courseCode (string) and storing course objects
+        public SortedList<string, Course> sortedList = new SortedList<string, Course>();
+
+
+        // indexer property allows array access to sortedList via the class object
+        // and catching missing keys and duplicate key exceptions 
+        // notice the indexer property definition shows how it will be used in the calling code:
+        // if we have:
+        //     Courses courses;
+        // then we can call:
+        //     Courses[courseCode] to access the course object with that email address
+        // and value will be the Course object (course) being added to the list in the case of:
+        //     courses[courseCode] = course;
+        public Course this[string courseCode]
+        {
+            get
+            {
+                Course returnVal;
+                try
+                {
+                    returnVal = (Course)sortedList[courseCode];
+                }
+                catch
+                {
+                    returnVal = null;
+                }
+
+                return (returnVal);
+            }
+
+            set
+            {
+                try
+                {
+                    // we can add to the list using these 2 methods
+                    //      sortedList.Add(courseCode, value);
+                    sortedList[courseCode] = value;
+                }
+                catch
+                {
+                    // an exception will be raised if an entry with a duplicate key is added
+                    // duplicate key handling
+                }
+            }
+
+        }
+        public void Remove(string courseCode)
+        {
+            if (courseCode != null)
+            {
+                sortedList.Remove(courseCode);
+            }
+        }
+
+        public Courses()
+        {
+            Course thisCourse;
+            Schedule thisSchedule;
+
+            Random rand = new Random();
+
+            // generate courses IGME-200 through IGME-299
+            for (int i = 200; i < 300; ++i)
+            {
+                // use constructor to create new course object with code and description
+                thisCourse = new Course(($"IGME-{i}"), ($"Description for IGME-{i}"));
+
+                // create a new Schedule object
+                thisSchedule = new Schedule();
+                for (int dow = 0; dow < 7; ++dow)
+                {
+                    // 50% chance of the class being on this day of week
+                    if (rand.Next(0, 2) == 1)
+                    {
+                        // add to the daysOfWeek list
+                        thisSchedule.daysOfWeek.Add((DayOfWeek)dow);
+
+                        // select random hour of day
+                        int nHour = rand.Next(0, 24);
+
+                        // set start and end times of minute duration
+                        // select fixed date to allow time calculations
+                        thisSchedule.startTime = new DateTime(1, 1, 1, nHour, 0, 0);
+                        thisSchedule.endTime = new DateTime(1, 1, 1, nHour, 50, 0);
+                    }
+                }
+
+                // set the schedule for this course
+                thisCourse.schedule = thisSchedule;
+
+                // add this course to the SortedList
+                this[thisCourse.courseCode] = thisCourse;
+            }
+        }
+
+
+    }
+
+    //Class: Course
+    //Author:Allison Maus
+    //Purpose: create course class to accept courseCode and description
+    //restrictions: none
+    public class Course
+    {
+        public string courseCode;
+        public string description;
+        public string teacherEmail;
+        public Schedule schedule;
+        //constructor Course
+        public Course(string courseCode, string description)
+        {
+            this.courseCode = courseCode;
+            this.description = description;
+        }
+    }
+
+
+
+
+
+}
+
---
+++ Week 07/Session 1/OperatorOverloadExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeopleLib;
+using CourseLib;
+
+namespace OperatorOverloadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.age = 23;
+            student1.gpa = 3.3;
+
+            student1.city = "Rochester";
+
+
+            Student student2 = new Student();
+            student1.age = 18;
+            student1.gpa = 3.5;
+
+            if( student1 < student2)
+            {
+                Console.WriteLine("student 1 has a lower gpa than student 2");
+            }
+
+            Person person1 = student1;
+            Person person2 = student2;
+
+            if( person1 < person2 )
+            {
+
+            }
+
+            if ((Person)student1 < (Student)student2)
+            {
+                Console.WriteLine("student 1 has a lower age than student 2");
+            }
+
+            Teacher teacher1 = new Teacher();
+            teacher1.age = 45;
+
+            if( (Person)teacher1 > (Person)student1 )
+            {
+                Console.WriteLine("teacher 1 has a higher age than student 1");
+            }
+
+        }
+    }
+}
